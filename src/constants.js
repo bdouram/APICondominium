ec3f1fd81d0c064674f12c9689ed059809f3af76
Bruno Miranda <bdouram@gmail.com>
2019-02-14 13:49:49 -0200

    :sparkles: Added a errors for CORS in mongoose

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const errors = {
+    required:  "'{PATH}' attribute is required.",
+    min: "The value '{VALUE}' is less then min value '{MIN}'.",
+    max: "The value '{VALUE}' is greather than the upper limit '{MAX}'."
+};
